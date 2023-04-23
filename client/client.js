f8d23db57a7626f0a9cce892284a76d0691c856a
Adam Seidman <adamjseidman@gmail.com>
2023-04-22 20:58:54 -0400

    Initial Commit

---
+++ client/client.js
@@ -0,0 +1,29 @@
+/**
+ * Author: Adam Seidman
+ * 
+ * Main entry point for Zack bot.
+ * 
+ */
+
+const { token, userID } = require('./private')
+const Discord = require('discord.js')
+
+
+// Bot Intentions
+const myIntents = ['GuildVoiceStates', 'GuildMessages', 'DirectMessages',
+    'MessageContent', 'GuildScheduledEvents']
+
+const bot = new Discord.Client({ intents: myIntents })
+bot.login(token)
+
+bot.on('ready')
+
+bot.on('voiceStateUpdate', (oldState, newState) => {
+    if (newState.channel) {
+        console.log('Connect.')
+    } else {
+        console.log('Disconnect.')
+    }
+    console.log(oldState)
+    console.log(newState)
+})
