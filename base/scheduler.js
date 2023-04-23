12f4e30c3d81baa688cb08fceec43165cd89470e
Adam Seidman <adamjseidman@gmail.com>
2023-04-23 18:38:45 -0400

    Create scheduler.js

---
+++ base/scheduler.js
@@ -0,0 +1,29 @@
+/**
+ * Author: Adam Seidman
+ * 
+ * Schedule the voice clips.
+ * 
+ * Exports:
+ *     scheduleVoiceEvents: Start schedule.
+ */
+
+const schedule = require('node-schedule')
+const state = require('./state')
+const { play } = require('./voice')
+
+const minSeconds = (60 * 15)
+const maxSeconds = (60 * 30)
+
+var scheduleCheck = function () {
+    var date = new Date()
+    var seconds = minSeconds + Math.ceil(Math.random() * (maxSeconds - minSeconds)) + 1
+    date.setSeconds(date.getSeconds() + seconds)
+    schedule.scheduleJob(date, async () => {
+        if (state.isConnected()) {
+            play(state.getChannel())
+        }
+        scheduleCheck()
+    })
+}
+
+module.exports = { scheduleVoiceEvents: scheduleCheck }
