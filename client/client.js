da7d0f2764d26e849fbe99d918cf54b92b78f4d1
Adam Seidman <adamjseidman@gmail.com>
2023-10-31 21:52:44 -0400

    Added a whole bunch of perms and bullying

--- client/client.js
+++ client/client.js
@@ -5,16 +5,17 @@
  * 
  */
 
-const { token, userID } = require('./private')
+const { token, userIdVoice, userIdMessage, replies } = require('./private')
 const Discord = require('discord.js')
 const state = require('../base/state')
 const { scheduleVoiceEvents } = require('../base/scheduler')
 
 // Bot Intentions
-const myIntents = ['GuildVoiceStates', 'GuildMessages', 'DirectMessages',
+const myIntents = ['Guilds', 'GuildVoiceStates', 'GuildMessages', 'DirectMessages',
     'MessageContent', 'GuildScheduledEvents']
+const myPartials = ['Channel']
 
-const bot = new Discord.Client({ intents: myIntents })
+const bot = new Discord.Client({ intents: myIntents, partials: myPartials })
 bot.login(token)
 
 bot.on('ready', () => {
@@ -23,7 +24,17 @@ bot.on('ready', () => {
 })
 
 bot.on('voiceStateUpdate', (oldState, newState) => {
-    if (newState.member.id === userID) {
+    if (newState.member.id === userIdVoice) {
         state.update(newState)
+    }
+})
+
+bot.on('messageCreate', msg => {
+    if ( msg.author !== undefined && `${msg.author.id}` === userIdMessage && msg.content.length > 6 ) {
+        if ( Math.ceil(Math.random() * 20) === 10 ) {
+            let response = replies[Math.floor(Math.random() * replies.length)]
+            console.log(`Replying to '${msg.content}' with '${response}'`)
+            msg.reply(response)
+        }
     }
 })
