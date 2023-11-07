c626880c055bba998eca180b90dd058463a1ce2a
Adam Seidman <adamjseidman@gmail.com>
2023-11-07 18:24:40 -0500

    Bother more Zachs with emojis

--- client/client.js
+++ client/client.js
@@ -5,15 +5,17 @@
  * 
  */
 
-const { token, userIdVoice, userIdMessage, replies } = require('./private')
+const { token, userIdVoice, userIdMessage, userIdEmoji, replies } = require('./private')
 const Discord = require('discord.js')
 const state = require('../base/state')
 const { scheduleVoiceEvents } = require('../base/scheduler')
+const emoji = require('emoji-dictionary')
 
 // Bot Intentions
 const myIntents = ['Guilds', 'GuildVoiceStates', 'GuildMessages', 'DirectMessages',
     'MessageContent', 'GuildScheduledEvents']
 const myPartials = ['Channel']
+
 
 const bot = new Discord.Client({ intents: myIntents, partials: myPartials })
 bot.login(token)
@@ -30,6 +32,17 @@ bot.on('voiceStateUpdate', (oldState, newState) => {
 })
 
 bot.on('messageCreate', msg => {
+    if ( msg.author !== undefined && `${msg.author.id}` === userIdEmoji ) {
+        if ( Math.ceil(Math.random() * 15) === 5 )
+        {
+            let times = Math.floor(Math.random() * 5) + 3
+            console.log(`Reacting ${times} times!`)
+            for ( let i = 0; i < times; i++ ) {
+                msg.react(emoji.unicode[Math.floor(Math.random() * emoji.unicode.length)])
+            }
+        }
+    }
+
     if ( msg.author !== undefined && `${msg.author.id}` === userIdMessage && msg.content.length > 6 ) {
         if ( Math.ceil(Math.random() * 20) === 10 ) {
             let response = replies[Math.floor(Math.random() * replies.length)]
